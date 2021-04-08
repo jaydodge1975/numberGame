6bbdee766720b35d1e7f14a5c2d467de86d98e33
Jay Dodge <jaydodge1975@eastlink.ca>
2021-04-07 19:07:18 -0600

    Added more setup files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,62 @@
+// quick and simple number guessing game from a course below
+
+// while(true) {
+//   let number = prompt("Guess the number:");
+//       number = Number(number);
+
+//   const randomNumber = Math.ceil(Math.random() * 10)
+
+//   if(number === randomNumber) {
+//     alert("YOU WIN");
+//     break;
+//   } else {
+//   console.log("You guessed", number, "But the right was", randomNumber);
+//   }
+// }
+
+var maxNumber = window.prompt("What should the maximum guess be?");
+
 while(true) {
-  let number = prompt("Guess the number:");
-      number = Number(number);
+   
+    if(maxNumber < 1) {
+        alert('Invalid Entry');
 
-  const randomNumber = Math.ceil(Math.random() * 10)
+        var maxNumber = prompt("Choose a positive number, greater than 0.");
 
-  if(number === randomNumber) {
-    alert("YOU WIN");
-    break;
-  } else {
-  console.log("You guessed", number, "But the right was", randomNumber);
-  }
+     } else if ( /^[0-9.,]+$/.test(maxNumber)) {
+
+            alert('Take a Guess between 1 and' + " " + maxNumber);
+     break;
+    }
+    }
+
+let random = Math.floor(Math.random()* maxNumber) + 1
+console.log(random)
+
+function userguess() {
+
+    let guessbox = Number(document.getElementById("guessboxvalue").value);
+
+let message = document.getElementById("message")
+
+     if(guessbox < 1 || guessbox > maxNumber) {
+        message.innerHTML = "That is not a number in range, try again!"
+
+            } else if(guessbox == random) {
+    message.innerHTML = "You got it!";
+
+}     else if(guessbox > random) {
+        message.innerHTML = "No, try a lower number!";
+
+    } else if (guessbox < random) {
+        message.innerHTML = "No, try a higher number!"; 
+    }
+    } 
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if ((charCode < 48 || charCode > 57))
+        alert("That is not a number.");
+
+    return true;
 }
